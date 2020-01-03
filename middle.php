78cfa11f8f159f4ab346d5124a4184b418bf9310
AlphaRedAlert <37663573+kmp59@users.noreply.github.com>
2020-01-03 13:49:05 -0500

    Added Existing Files

---
+++ middle.php
@@ -0,0 +1,19 @@
+<?php
+if(isset($_POST['UCID'], $_POST['password'])){
+    $ucid = ($_POST['UCID']);
+    $password = $_POST['password'];
+} 
+$data = array('UCID'=>$ucid, 'password'=>$password);
+//echo $data['UCID'];
+$string = http_build_query($data);
+$send = curl_init();
+curl_setopt($send, CURLOPT_URL, "https://web.njit.edu/~ap2257/BackEnd/backend.php");
+curl_setopt($send, CURLOPT_POST, true);
+curl_setopt($send, CURLOPT_POSTFIELDS, $string);
+curl_setopt($send, CURLOPT_RETURNTRANSFER, true);
+$resp = curl_exec($send); 
+curl_close($send); 
+//$jsonDecoded = json_decode($resp);
+//echo $jsonDecoded; 
+echo $resp; 
+?>
